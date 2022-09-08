e78a18e4193f8e4f893ed3b2e197c712a87d4161
motun marteen <motunmarteen@gmail.com>
2022-09-08 07:39:50 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * Deacription: main - Prints out a string to stdout.
+ * Return: 0 if success.
  */
-int main(void)
+jnt main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multiligual puzzle");
 	return (0);
 }
