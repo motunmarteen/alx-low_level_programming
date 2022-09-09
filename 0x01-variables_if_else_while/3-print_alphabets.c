91f349b00e41d718a8b35d12806a881632a56eb2
motun marteen <motunmarteen@gmail.com>
2022-09-09 16:45:18 -0700

    3. alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,7 +8,7 @@ int main(void)
 {
 	char letter;
 
-	for (letter = 'a'; letter <= 'I'; letter++)
+	for (letter = 'a'; letter <= 'z'; letter++)
 		putchar(letter);
 	for (letter = 'A'; letter <= 'Z'; letter++)
 		putchar(letter);
