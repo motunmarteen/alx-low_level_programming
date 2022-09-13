d3e7ea2b71f5cbec24518c9e5de84070c4bc1e02
motun marteen <motunmarteen@gmail.com>
2022-09-13 15:04:32 -0700

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,21 @@
-#include <main.h>
+#include "main.h"
 
 /**
- * main - Entry point
+ * main - prints putchar
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	putchar('_');
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
-	putchar('\n');
+	_putchar('_');
+	_putcahr('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n';
+
 	return (0);
 }
