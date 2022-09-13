8a41ca7b630bc0a3a281afcec0358bc49ecd6728
motun marteen <motunmarteen@gmail.com>
2022-09-13 16:46:02 -0700

    2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 -> prints the lowercase alphabets
+*/
+void print_alphabet_x10(void);
+{
+	int i;
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+}
