fa339dea64c8a3c6668a37795aa815d509be46ae
motun marteen <motunmarteen@gmail.com>
2022-09-14 08:09:14 -0700

    16. In computer class, the first assignment was to write a program to print the first 100 Fibonacci numbers. Instead, I wrote a program that would steal passwords of students. My teacher gave me an A+

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int fibNum;
+	unsigned long fibOne = 0, fibTwo = 1, allSum;
+	unsigned long fibOne_halfOne, fibOne_halfTwo, fibTwo_halfOne, fibTwo_halfTwo;
+	unsigned long halfOne, halfTwo;
+
+	for (fibNum = 0; fibNum < 92; fibNum++)
+	{
+		allSum = fibOne + fibTwo;
+		printf("%lu, ", allSum);
+
+		fibOne = fibTwo;
+		fibTwo = allSum;
+	}
+	fibOne_halfOne = fibOne / 10000000000;
+	fibTwo_halfOne = fibTwo / 10000000000;
+	fibOne_halfTwo = fibOne % 10000000000;
+	fibTwo_halfTwo = fibTwo % 10000000000;
+
+	for (fibNum = 93; fibNum < 99; fibNum++)
+	{
+		halfOne = fibOne_halfOne + fibTwo_halfOne;
+		halfTwo = fibOne_halfTwo + fibTwo_halfTwo;
+		if (fibOne_halfTwo + fibTwo_halfTwo > 9999999999)
+		{
+			halfOne += 1;
+			halfTwo %= 10000000000;
+		}
+
+		printf("%lu%lu", halfOne, halfTwo);
+		if (fibSum != 98)
+		{
+			printf(", ");
+		}
+		fibOne_halfOne = fibTwo_halfOne;
+		fibOne_halfTwo = fibTwo_halfTwo;
+		fibTwo_halfOne = halfOne;
+		fibTwo_halfTwo = halfTwo;
+	}
+	printf("\n");
+	return (0);
+}
