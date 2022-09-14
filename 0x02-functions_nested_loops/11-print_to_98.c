9e9f681c6ec8e750f40f1b9faa70998c8bccb9a9
motun marteen <motunmarteen@gmail.com>
2022-09-14 06:03:38 -0700

    11. 98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+ * print_to_98 -> a function that prints all natural numbers from n to 98
+ * user inputs number prints to 98, regardless < 98 or > 98
+ *
+ * @n: number input
+ * Return: Always 0 (Success)
+ */
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		while (n <= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(",");
+			}
+			n++;
+		}
+	}
+	else if (n > 98)
+	{
+		while (n >= 98)
+		{
+			printf("%d", n)
+			if (n != 98)
+			{
+				printf(",");
+			}
+			n--;
+		}
+	}
+	else
+	{
+		printf("98");
+	}
+	printf("\n");
+}
