cc1de44ecf432bcae71bd624bdfc77c261aeaace
motun marteen <motunmarteen@gmail.com>
2022-09-14 05:50:46 -0700

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add -> a function that adds two integers and retruns the result
+ * @one: first number input
+ *
+ * @two: second number input
+ * Return: one + two
+ */
+int add(int one, int two)
+{
+	return (one + two);
+}
