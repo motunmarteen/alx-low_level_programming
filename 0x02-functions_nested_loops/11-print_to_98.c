7260eb39518e1d31f8e3a38b7a9ad035fb70f5cf
motun marteen <motunmarteen@gmail.com>
2022-09-14 06:10:08 -0700

    hot fix

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -17,7 +17,7 @@ void print_to_98(int n)
 			printf("%d", n);
 			if (n != 98)
 			{
-				printf(",");
+				printf(", ");
 			}
 			n++;
 		}
@@ -29,7 +29,7 @@ void print_to_98(int n)
 			printf("%d", n);
 			if (n != 98)
 			{
-				printf(",");
+				printf(", ");
 			}
 			n--;
 		}
