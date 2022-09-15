0bb16873bae66321ad0940268f7d5543017bb244
motun marteen <motunmarteen@gmail.com>
2022-09-15 15:57:10 -0700

    0 > 972?

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,18 +12,7 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a >= b && a >= c)
-	{
-		largest = a;
-	}
-	else if (b >= a && b >= c)
-	{
-		largest = b;
-	}
-	else
-	{
-		largest = c;
-	}
-
+	largest = (a > b) ? a : b;
+	largest = (largest > c) ? largest : c;
 	return (largest);
 }
