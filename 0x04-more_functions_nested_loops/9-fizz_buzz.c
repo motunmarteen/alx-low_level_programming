0b9c55d4162d6ac198e40b930aba6226bb08eaf6
motun marteen <motunmarteen@gmail.com>
2022-09-15 10:26:08 -0700

    9. Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main -> Entry point
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 1; num <= 100; num++)
+	{
+		if ((num % 3) == 0 && (num % 5) == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if ((num % 3) == 0)
+		{
+			printf("Fizz");
+		}
+		else if ((num % 5) == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+				printf("%d", num);
+		}
+	}
+	printf("\n");
+
+	return (0);
+}
