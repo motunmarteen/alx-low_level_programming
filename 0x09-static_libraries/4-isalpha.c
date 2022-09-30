8b324af9837dd82a3ec21f751985e9e17cd83ed6
motun marteen <motunmarteen@gmail.com>
2022-09-30 12:02:54 -0700

    Create the static library libmy.a containing all the functions listed below:

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isalpha -> a function that checks for alphabetic character
+ * @c: single letter input
+ * Return: 1 if c is a letter (lower or uppercase), 0 otherwise
+ */
+int _isalpha(int c)
+{
+	return (((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')));
+}
---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,1 @@
+This folder is contains C Libraries static files
