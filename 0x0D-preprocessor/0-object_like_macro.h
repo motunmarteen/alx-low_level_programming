1ea1e27cff89e5dee62ac3e3b113c75537fb4eb2
motun marteen <motunmarteen@gmail.com>
2022-10-09 23:37:36 -0700

    Create a header file that defines a macro named SIZE as an abbreviation for the token 1024.

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,13 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+/*
+ * Filename: 0-object_like_macro.h
+ * Creator: Motun Marteen
+ * Description: Header file that defines a macro named
+ *       SIZE as an abbreviation for the token 1024.
+ */
+
+#define SIZE 1024
+
+#endif
