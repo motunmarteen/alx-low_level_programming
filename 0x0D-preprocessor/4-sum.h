28914910cf59a540c875a97064129560dfa7c136
motun marteen <motunmarteen@gmail.com>
2022-10-09 23:50:04 -0700

    Write a function-like macro SUM(x, y) that computes the sum of the numbers x and y.

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,13 @@
+#ifndef SUM_H
+#define SUM_H
+
+/*
+ * Filename: 4-sum.h
+ * Creator: Motun Marteen
+ * Description: Header file that defines a function-like macro SUM(x, y)
+ *       that computes the sum of the numbers x and y.
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
