c76a237dfe7c7d17ec50771677f76d93ef2cfb84
motun marteen <motunmarteen@gmail.com>
2022-10-09 23:40:30 -0700

    Create a header file that defines a macro named PI as an abbreviation for the token 3.14159265359.

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,13 @@
+#ifndef PI_H
+#define PI_H
+
+/*
+ * Filename: 1-pi.h
+ * Creator: Brennan D Baraban
+ * Description: Header file that defines a macro named PI
+ *       as an abbreviation for the token 3.14159265359.
+ */
+
+#define PI 3.14159265359
+
+#endif
